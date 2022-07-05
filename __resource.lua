60497925942cc2c8940535c930c3219071d7751e
DJ On Top <107875648+djontop@users.noreply.github.com>
2022-07-05 19:56:24 +0530

    Create __resource.lua

---
+++ __resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+version '1.0.0'
+author 'DJ ✔ 👑#5537'
+description 'Show Server uptime And Webhook Supported Server Status'
+repository 'https://github.com/djontop'
+
+server_script "uptime.lua"
+shared_script "config.lua"

5340361fb7cf299db99c3552db694e8d17df19c5
DJ On Top <107875648+djontop@users.noreply.github.com>
2022-07-05 19:55:48 +0530

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,24 @@
+------------------------https://github.com/djontop-------------------------------
+Config = {}
+Config.Webhook = ''
+Config.ServerName = 'Server Name What You Want To Keep'
+
+Config.MessageId = '' --Copy messageid from deployed message and restart script!
+
+Config.UpdateTime = 1000*60*1 -- 1 minute
+Config.JoinLink = 'Joining Link' -- Make sure that JoinLink is URL, like: https://cfx.re/join/xp34mg, currenlty does not support Redm
+
+Config.EmbedColor = 15548991
+
+Config.Locale = 'en'
+
+Config.Locales = {
+    ['en'] = {
+        ['date'] = 'Last Refresh:   Date',
+        ['time'] = 'Time',
+        ['players'] = 'Players',
+        ['connect'] = 'Connect to server',
+    }
+}
+
+------------------------https://github.com/djontop-------------------------------
